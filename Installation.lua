5ed7cc3569173e425e49d93d76fc3d829f375141
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2023-06-19 00:47:48 +0100

    1.0.2

---
+++ Installation.lua
@@ -0,0 +1,34 @@
+--[[
+
+	LL			IIIIIIII	NNN    NN	TTTTTTTTTTT
+	LL			   II		NN N   NN		TT
+	LL			   II		NN  N  NN		TT
+	LL			   II		NN   N NN		TT
+	LLLLLLLL	IIIIIIII	NN    NNN		TT
+	
+	 ..
+	....
+	 ..
+	 
+	 AAAAAAAA	CCCCCCCC
+	 AA	   AA	CC
+	 AAAAAAAA	CC
+	 AA	   AA	CC
+	 AA	   AA   CCCCCCCC
+	 
+	 Advanced Anti-Cheat
+	 Want to send hackers to the shadow realm for free? Use lint today!
+	 RBLX: @HiRobloxDown | DISCORD: Moonzy#0001
+	 
+	 1.0.2
+	 
+	 Configure the Anti-Cheat below this script
+	 We have recently moved from 1 script to different scripts for each module.
+	 If you are installing Lint.AC from GitHub please use the link inside "download.txt".
+	 This update shall make it easier to update our Anti-Cheat, aswell as making it perform better.
+	 Thank you!
+	 - Lint Development Team
+   
+   Install from this model: https://www.roblox.com/library/13794619164/Lint-AntiCheat-v1-0-2
+
+]]
